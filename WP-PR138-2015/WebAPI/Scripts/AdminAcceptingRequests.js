6920ce562a6235ec62ee23b5894b82eef7974e79
serbulas1 <serbulas1@gmail.com>
2018-06-18 18:53:23 +0200

    ne mogu dva admina istu voznju da dodeljuju

--- WP-PR138-2015/WebAPI/Scripts/AdminAcceptingRequests.js
+++ WP-PR138-2015/WebAPI/Scripts/AdminAcceptingRequests.js
@@ -43,7 +43,7 @@
     $("#olforcrtreqadm").delegate("#btnaccrequestadm", "click", function (e) {
         let index = $(this).parent().index();   //zelim ID trenutnog <li>
         let temp = $('#olforcrtreqadm').find(`li:eq(${index})`).html(); //da pronadjem tekst iz tacno oznacenog <li>
-        
+
         let info = temp.split('"');
         let id = info[5];      //id voznje
         id = parseInt(id);
@@ -85,115 +85,142 @@
         });
     });
 
+    $('.popup-close').on('click', function (e) {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+    });
     //----- Create
-    $('[data-popup-close]').on('click', function (e) {
+    $('#popcrt').on('click', function (e) {
         var targeted_popup_class = jQuery(this).attr('data-popup-close');
         $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
 
         let admin = JSON.parse(sessionStorage.getItem('logged'));
         let id = JSON.parse(sessionStorage.getItem('driveId'));
         let driver = $('#driversadm2').val();
+        let status = true;
 
-        let send = {
-            Id: id,
-            DriverID: driver,
-            AdminID: admin.Username
-        }
+        $.when(
+            $.ajax({                        //da vratim trenutno stanje ulogovanog   
+                method: "GET",
+                url: "/api/Registration",
+                data: { id: id },
+                dataType: "json",
+                success: function (data) {
+                    if (data.StatusString !== 'Created') {
+                        status = false;
+                    }
+                },
+                error: function (msg) {
+                    alert("Fail - " + msg.responseText);
+                }
+            }),
+        ).then(function () {
+            if (status) {
+                let send = {
+                    Id: id,
+                    DriverID: driver,
+                    AdminID: admin.Username
+                }
 
-        $.ajax({        //menjamo statuse voznje, vozaca - Processed
-            method: "PUT",
-            url: "/api/Smart3",
-            data: JSON.stringify(send),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function () {
-                alert('Drive assigned');
-                
-                $.ajax({                    //uzimam sve voznje, ali cu priokazati samo od ovog admina
-                    method: "GET",
-                    url: "/api/Voznja",
+                $.ajax({        //menjamo statuse voznje, vozaca - Processed
+                    method: "PUT",
+                    url: "/api/Smart3",
+                    data: JSON.stringify(send),
+                    contentType: "application/json; charset=utf-8",
                     dataType: "json",
-                    success: function (response) {
-                        $("#lblhome").empty();
-                        $('#lblhome').append('================Drives=====================');
-                        let startLoc;
-                        let endLoc;
-                        let comments = [];
+                    success: function () {
+                        alert('Drive assigned');
 
-                        $.each(response, function (index, value) {
-                            if (value.AdminID != null && value.AdminID == admin.Username) {
-                                $.when(
-                                    $.ajax({                    //za svaku voznju vracam pocetnu lokaciju posebno
-                                        method: "GET",
-                                        url: "/api/Address",
-                                        data: { id: value.StartPointID },
-                                        dataType: "json",
-                                        success: function (loc) {
-                                            startLoc = loc;
+                        $.ajax({                    //uzimam sve voznje, ali cu priokazati samo od ovog admina
+                            method: "GET",
+                            url: "/api/Voznja",
+                            dataType: "json",
+                            success: function (response) {
+                                $("#lblhome").empty();
+                                $('#lblhome').append('================Drives=====================');
+                                let startLoc;
+                                let endLoc;
+                                let comments = [];
 
-                                            if (value.FinalPointID != null) {
-                                                $.ajax({                    //za svaku voznju vracam krajnju lokaciju posebno, ako postoji
-                                                    method: "GET",
-                                                    url: "/api/Address",
-                                                    data: { id: value.FinalPointID },
-                                                    dataType: "json",
-                                                    success: function (floc) {
-                                                        endLoc = floc;
-                                                    },
-                                                    error: function (msg) {
-                                                        alert("Fail - " + msg.responseText);
+                                $.each(response, function (index, value) {
+                                    if (value.AdminID != null && value.AdminID == admin.Username) {
+                                        $.when(
+                                            $.ajax({                    //za svaku voznju vracam pocetnu lokaciju posebno
+                                                method: "GET",
+                                                url: "/api/Address",
+                                                data: { id: value.StartPointID },
+                                                dataType: "json",
+                                                success: function (loc) {
+                                                    startLoc = loc;
+
+                                                    if (value.FinalPointID != null) {
+                                                        $.ajax({                    //za svaku voznju vracam krajnju lokaciju posebno, ako postoji
+                                                            method: "GET",
+                                                            url: "/api/Address",
+                                                            data: { id: value.FinalPointID },
+                                                            dataType: "json",
+                                                            success: function (floc) {
+                                                                endLoc = floc;
+                                                            },
+                                                            error: function (msg) {
+                                                                alert("Fail - " + msg.responseText);
+                                                            }
+                                                        });
                                                     }
+                                                },
+                                                error: function (msg) {
+                                                    alert("Fail - " + msg.responseText);
+                                                }
+                                            }),
+
+                                            $.ajax({                    //za svaku voznju vracam komentare, ukoliko su npr kom i vozac i musterija
+                                                method: "GET",
+                                                url: "/api/Smart2",
+                                                data: { startLocation: value.StartPointID },
+                                                dataType: "json",
+                                                success: function (loc) {
+                                                    comments = loc;
+                                                },
+                                                error: function (msg) {
+                                                    alert("Fail - " + msg.responseText);
+                                                }
+                                            }),
+                                        ).then(function () {
+                                            $('#lblhome').append(`<br />Driver: ${value.DriverID} - Car type: ${value.TypeString}`);
+                                            if (value.UserCallerID != null) {
+                                                $('#lblhome').append(`<br />Customer: ${value.UserCallerID}`);
+                                            }
+                                            $('#lblhome').append(`<br />From: ${startLoc} - To: ${endLoc}`);
+                                            $('#lblhome').append(`<br />Status: ${value.StatusString} - Reservation time: ${value.TimeOfReservation}`);
+                                            if (value.Payment != null) {
+                                                $('#lblhome').append(`<br />Payment: ${value.Payment}`);
+                                            }
+                                            if (comments.length > 0) {
+                                                $.each(comments, function (index, value) {
+                                                    $('#lblhome').append(`<br />Comment posted by: ${value.UserID} - Time: ${value.PostingTime}`);
+                                                    $('#lblhome').append(`<br />Grade for this ride: ${value.Grade}`);
+                                                    $('#lblhome').append(`<br /><br /><textarea readonly rows="8" cols="35">${value.Description}</textarea>`);
                                                 });
                                             }
-                                        },
-                                        error: function (msg) {
-                                            alert("Fail - " + msg.responseText);
-                                        }
-                                    }),
-
-                                    $.ajax({                    //za svaku voznju vracam komentare, ukoliko su npr kom i vozac i musterija
-                                        method: "GET",
-                                        url: "/api/Smart2",
-                                        data: { startLocation: value.StartPointID },
-                                        dataType: "json",
-                                        success: function (loc) {
-                                            comments = loc;
-                                        },
-                                        error: function (msg) {
-                                            alert("Fail - " + msg.responseText);
-                                        }
-                                    }),
-                                ).then(function () {
-                                    $('#lblhome').append(`<br />Driver: ${value.DriverID} - Car type: ${value.TypeString}`);
-                                    if (value.UserCallerID != null) {
-                                        $('#lblhome').append(`<br />Customer: ${value.UserCallerID}`);
-                                    }
-                                    $('#lblhome').append(`<br />From: ${startLoc} - To: ${endLoc}`);
-                                    $('#lblhome').append(`<br />Status: ${value.StatusString} - Reservation time: ${value.TimeOfReservation}`);
-                                    if (value.Payment != null) {
-                                        $('#lblhome').append(`<br />Payment: ${value.Payment}`);
-                                    }
-                                    if (comments.length > 0) {
-                                        $.each(comments, function (index, value) {
-                                            $('#lblhome').append(`<br />Comment posted by: ${value.UserID} - Time: ${value.PostingTime}`);
-                                            $('#lblhome').append(`<br />Grade for this ride: ${value.Grade}`);
-                                            $('#lblhome').append(`<br /><br /><textarea readonly rows="8" cols="35">${value.Description}</textarea>`);
+                                            $('#lblhome').append('<br />===========================================');
                                         });
                                     }
-                                    $('#lblhome').append('<br />===========================================');
                                 });
+                                $('#divallreqcreatedadm').hide();
+                                $('#divhome').show();
+                            },
+                            error: function (msg) {
+                                alert("Fail - " + msg.responseText);
                             }
                         });
-                        $('#divallreqcreatedadm').hide();
-                        $('#divhome').show();
                     },
                     error: function (msg) {
                         alert("Fail - " + msg.responseText);
                     }
                 });
-            },
-            error: function (msg) {
-                alert("Fail - " + msg.responseText);
+            } else {
+                alert('Drive was assigned by another admin, try another');
+                $('#btnacptreqadm').trigger('click');
             }
         });
     });
